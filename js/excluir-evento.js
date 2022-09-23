bc16a239b3a2c616bbaa8ad7b4617c87a5987da7
elenbrodt <elenbrodt@gmail.com>
2022-09-23 12:31:46 -0300

    formatacao do codigo

--- js/excluir-evento.js
+++ js/excluir-evento.js
@@ -5,80 +5,73 @@ const descricaoEvento = document.querySelector("#descricao");
 const dataEvento = document.querySelector("#data");
 const lotacaoEvento = document.querySelector("#lotacao");
 
-function zerarCampos () {
-    nomeEvento.setAttribute("value", "");
-    bannerEvento.setAttribute("value", "");
-    atracoesEvento.setAttribute("value", "");
-    descricaoEvento.innerHTML="";
-    dataEvento.setAttribute("value", "");
-    lotacaoEvento.setAttribute("value", "");
+function zerarCampos() {
+  nomeEvento.setAttribute("value", "");
+  bannerEvento.setAttribute("value", "");
+  atracoesEvento.setAttribute("value", "");
+  descricaoEvento.innerHTML = "";
+  dataEvento.setAttribute("value", "");
+  lotacaoEvento.setAttribute("value", "");
 }
 
 zerarCampos();
 
-const urlId = (window.location.search).split("?")[1]
+const urlId = window.location.search.split("?")[1];
 
 const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com";
 
-const btnExcluir = document.querySelector(".btn-danger")
+const btnExcluir = document.querySelector(".btn-danger");
 
-var nomeEventoExcluido = ""
+var nomeEventoExcluido = "";
 
-async function excluirEventosPagina () {
-    try{
-        const endPoint = await fetch(`${BASE_URL}/events`);
-        const newEndPoint = await endPoint.json();
+async function excluirEventosPagina() {
+  try {
+    const endPoint = await fetch(`${BASE_URL}/events`);
+    const newEndPoint = await endPoint.json();
 
-        console.log("endpoint sem excluir:", newEndPoint)
+    newEndPoint.forEach((evento) => {
+      if (evento._id == urlId) {
+        const data = ajusteHorario(evento.scheduled);
 
-        newEndPoint.forEach((evento)=>{
-            if (evento._id == urlId){
+        nomeEvento.setAttribute("value", `${evento.name}`);
+        bannerEvento.setAttribute("value", `${evento.poster}`);
+        atracoesEvento.setAttribute("value", `${evento.attractions}`);
+        descricaoEvento.innerHTML = `${evento.description}`;
+        dataEvento.setAttribute("value", `${data}`);
+        lotacaoEvento.setAttribute("value", `${evento.number_tickets}`);
 
-                const data = ajusteHorario(evento.scheduled)
-                console.log(data)
-                nomeEvento.setAttribute("value", `${evento.name}`);
-                bannerEvento.setAttribute("value", `${evento.poster}`);
-                atracoesEvento.setAttribute("value", `${evento.attractions}`);
-                descricaoEvento.innerHTML=`${evento.description}`;
-                dataEvento.setAttribute("value", `${data}`);
-                lotacaoEvento.setAttribute("value", `${evento.number_tickets}`);
-
-                nomeEventoExcluido = `${evento.name}`;
-            }
-        })
-    } 
-    catch (error){
-        console.log(error)
-    }
-}
-
-function ajusteHorario (evento){
-
-    const offset = new Date(evento).getTimezoneOffset() * 1000 * 60;
-    const dataValor = new Date(evento).valueOf();
-    const data = new Date(dataValor - offset).toISOString().substr(0,16);
-  
-    return data
+        nomeEventoExcluido = `${evento.name}`;
+      }
+    });
+  } catch (error) {
+    console.log(error);
   }
-  
-excluirEventosPagina()
-
-async function excluirParaSempre(){
-    try{
-        const response = await fetch( `${BASE_URL}/events/${urlId}`,{
-            method: "DELETE",
-        });
-        console.log(response.status)
-        console.log('excluiu')
-    }
-    catch(error){
-        console.log(error)
-    }
 }
 
-btnExcluir.onclick = (event) =>{
-    event.preventDefault()
-    excluirParaSempre();
-    alert(`Você excluiu o evento ${nomeEventoExcluido}`)
-    window.location.assign("admin.html")
+function ajusteHorario(evento) {
+  const offset = new Date(evento).getTimezoneOffset() * 1000 * 60;
+  const dataValor = new Date(evento).valueOf();
+  const data = new Date(dataValor - offset).toISOString().substr(0, 16);
+
+  return data;
 }
+
+excluirEventosPagina();
+
+async function excluirParaSempre() {
+  try {
+    const response = await fetch(`${BASE_URL}/events/${urlId}`, {
+      method: "DELETE",
+    });
+    console.log(response.status);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+btnExcluir.onclick = (event) => {
+  event.preventDefault();
+  excluirParaSempre();
+  alert(`Você excluiu o evento ${nomeEventoExcluido}`);
+  window.location.assign("admin.html");
+};
